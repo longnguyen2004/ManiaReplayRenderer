9c73eeb8902efc26abfb542be57193dcc0fd8de6
Long Nguyen <nguyen.long.908132@gmail.com>
2020-11-03 17:36:37 +0700

    [Main] Test scroll speed and barline drawer

--- Renderer/src/main.cpp
+++ Renderer/src/main.cpp
@@ -27,8 +27,8 @@ int main(int argc, char const *argv[])
     std::locale::global(std::locale(".65001"));
 #endif
 
-    std::string mapFile(
-        "G:/osu test/Proluvies/Camellia - Proluvies (TheToaphster) [SVecial].osu");
+    std::string mapFile("G:/osu test/407153 LeaF - Doppelganger/LeaF - Doppelganger "
+                        "(Jinjin) [Zen's Insane].osu");
     Map testMap(mapFile);
     std::cout << "[Main] Map information: \n";
     std::cout << "[Main] Title: " << testMap.getMetadata()["Title"] << '\n';
@@ -37,7 +37,7 @@ int main(int argc, char const *argv[])
     std::string skinFolder(
         "C:/Users/nghuu/AppData/Local/osu!/Skins/R Skin v3.0 (Bars)");
     Skin testSkin(skinFolder);
-    double scrollSpeed = 30.0;
+    double scrollSpeed = 33.0;
 
     unsigned int width = 1280;
     unsigned int height = 720;
